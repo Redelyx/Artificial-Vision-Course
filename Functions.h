6e652d963233844c8a1e208eeeed75204b7528ff
redelyx <alice.lv@live.it>
2022-11-10 19:05:16 +0100

    Lab 1a and 2 added

--- Functions.h
+++ Functions.h
@@ -1,21 +1,20 @@
-#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 
-//std:
+// std:
+#include <ctime> // std::time
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <ctime>        // std::time
+#include <vector>
 
-//getopt()
+// getopt()
 #include <unistd.h>
 
-
 //---UTILITY---
-unsigned char openandwait(const char *windowname, cv::Mat &img, const bool sera);
-
+unsigned char openandwait(const char* windowname, cv::Mat& img, const bool sera);
 
 //---LAB 1---
 cv::Mat downsampling2x(const cv::Mat image);
@@ -30,8 +29,13 @@ cv::Mat colorShuffle(const cv::Mat image);
 void sample(const cv::Mat image, std::string image_name);
 
 //---LAB 1a---
+cv::Mat myfilter2D(const cv::Mat src, const cv::Mat& krn, cv::Mat& out, int stride);
 
-cv::Mat myfilter2D(const cv::Mat src, const cv::Mat& krn, cv::Mat& out,  int stride);
+//---LAB 2---
+
+cv::Mat simpleBgSubtraction(const cv::Mat image, int threshold);
+cv::Mat runningAverageBgSubtraction(const cv::Mat image, std::vector<cv::Mat>& v, int threshold, int n);
+cv::Mat expRunningAverageBgSubtraction(const cv::Mat image, int threshold, float alpha);
 
 //---LAB 3---
 
